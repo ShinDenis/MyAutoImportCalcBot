c1e2f2f335ddf76d50b403e1c01a8139ef43e140
SHIN DENIS <deniskaalwaysawesome@gmail.com>
2026-04-30 08:47:34 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -34,7 +34,7 @@ main_kb = ReplyKeyboardMarkup(
 @dp.message(Command("start"))
 async def start(message: types.Message):
     await message.answer(
-        "Вас приветствует Бот калькулятор! Выберите команду из меню или введите модель и цену через пробел: n\Например: AUDI 5000",
+        "Вас приветствует Бот калькулятор! Выберите команду из меню или введите модель и цену через пробел: \n Например: AUDI 5000",
         reply_markup=main_kb
     )
 
